f0890f3296ac912a0dff022a4f5fcff939bf8629
zzuo123 <33331699+zzuo123@users.noreply.github.com>
2021-11-20 22:31:24 -0500

    Added led strip rainbow mode

--- LED_strip/LED_strip.ino
+++ LED_strip/LED_strip.ino
@@ -1,12 +1,12 @@
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN     12
-#define LED_COUNT   31
+#define LED_COUNT   144
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin();           // INITIALIZE NeoPixel strip object
   strip.show();            // Turn OFF all pixels
-  strip.setBrightness(20); // Set BRIGHTNESS of all pixels(max = 255)
+  strip.setBrightness(5); // Set BRIGHTNESS of all pixels(max = 255)
 }
 
 // Global Variables for helper functions
@@ -59,15 +59,44 @@ void colorBounce(uint32_t color, int len, int wait) {
   }
 }
 
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i*1+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } 
+  else if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } 
+  else {
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
 void loop() {
-  colorWipe(strip.Color(255,   0,   0), 50); // Red
-  colorWipe(strip.Color(0,   255,   0), 50); // Green
-  colorWipe(strip.Color(0,   0,   255), 50); // Blue
-  colorWipe(strip.Color(0,   0,   0), 50); // Off
-  colorSeg(strip.Color(255,   0,   0), 3, 50); // Red
-  colorSeg(strip.Color(0,   255,   0), 3, 50); // Green
-  colorSeg(strip.Color(0,   0,   255), 3, 50); // Blue
-  colorBounce(strip.Color(255,   0,   0), 3, 50); // Red
-  colorBounce(strip.Color(0,   255,   0), 3, 50); // Green
-  colorBounce(strip.Color(0,   0,   255), 3, 50); // Blue
+//  colorWipe(strip.Color(255,   0,   0), 20); // Red
+//  colorWipe(strip.Color(0,   255,   0), 20); // Green
+//  colorWipe(strip.Color(0,   0,   255), 20); // Blue
+//  colorWipe(strip.Color(0,   0,   0), 20); // Off
+//  colorSeg(strip.Color(255,   0,   0), 30, 20); // Red
+//  colorSeg(strip.Color(0,   255,   0), 30, 20); // Green
+//  colorSeg(strip.Color(0,   0,   255), 30, 20); // Blue
+//  colorBounce(strip.Color(255,   0,   0), 30, 20); // Red
+//  colorBounce(strip.Color(0,   255,   0), 30, 20); // Green
+//  colorBounce(strip.Color(0,   0,   255), 30, 20); // Blue
+  rainbow(30);
+  delay(10);
 }
