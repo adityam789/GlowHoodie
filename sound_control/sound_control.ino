5d9d184f0ad279f314859c40fda672018d85af89
zzuo123 <33331699+zzuo123@users.noreply.github.com>
2021-12-06 21:38:00 -0500

    Fix sound, add animation, rename MASTER

--- sound_control/sound_control.ino
+++ sound_control/sound_control.ino
@@ -1,4 +1,4 @@
-uint8_t soundSensorPin=15;
+uint8_t soundSensorPin=33;
 uint8_t neoPin = 2;
 unsigned long lastSound = 0;
 
@@ -21,20 +21,20 @@ void setup() {
 //}
 
 void loop() {
-//  uint16_t val = analogRead(soundSensorPin);
-//  unsigned long now = millis();
-//  Serial.println(val);
+  unsigned long val = analogRead(soundSensorPin);
+  unsigned long now = millis();
+  Serial.println(val);
 //  add debouncing and LED color control
 //  delay(10);
-  if(val >= 2500 && now > lastSound + 500) {
+  if((val >= 1900 || val <= 1820) && now > lastSound + 1000) {
 //    Serial.print(now);
 //    Serial.print(": ");
 //    Serial.println("I heard it");
-//    digitalWrite(neoPin,!digitalRead(neoPin)); // toggles the neopixel
+    digitalWrite(neoPin,!digitalRead(neoPin)); // toggles the neopixel
 //    Serial.println(sizeof(uint32_t));
-//    lastSound = now;
-    Serial.print("in loop: ");
-    Serial.println(millis());
-    delay(1000);
+    lastSound = now;
+//    Serial.print("in loop: ");
+//    Serial.println(millis());
+//    delay(1000);
   }
 } 
